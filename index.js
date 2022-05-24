55fd7f3b5695e597e37b20847f371ea035035143
cagilocal <105216712+cagilocal@users.noreply.github.com>
2022-05-24 04:32:38 +0300

    JS file added

---
+++ index.js
@@ -0,0 +1,54 @@
+let a = 3;
+let b = 5;
+let c = undefined
+
+alert(a + b);
+
+alert(a - b);
+
+alert(a * b);
+
+alert(a / b);
+
+alert(a % b);
+
+alert(a += b);
+
+alert(a -= b);
+
+alert(a *= b);
+
+alert(a /= b);
+
+alert(a %= b);
+
+alert(a == b);
+
+alert(a != b);
+
+alert(a > b);
+
+alert(a < b);
+
+alert(!a && !c);
+
+alert(!a || !c);
+
+
+let first_name = 'Cagil';
+
+let last_name = 'Ocal';
+
+let email = 'ocal0012@algonquinlive.com';
+
+let experiment;
+
+experiment = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`
+
+alert(experiment);
+
+
+
+
+ 
+ 
